6961fdea265f08d0125a5252c17f706e46e46076
mtarifaagro <40060725+mtarifaagro@users.noreply.github.com>
2018-06-12 14:51:16 +0000

    Create config.inc.php

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -28,7 +28,9 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */ 
-$cfg['Servers'][$i]['host'] = '172.30.133.27:3306';
+ 
+$cfg['Servers'][$i]['host'] = getenv('MYSQL_SERVICE_HOST');
+$cfg['Servers'][$i]['port'] = getenv('MYSQL_SERVICE_PORT'); 
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
