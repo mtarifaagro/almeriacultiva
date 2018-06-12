2def50af89737028e1f29a52cc90ba50a3f4c9f9
mtarifaagro <40060725+mtarifaagro@users.noreply.github.com>
2018-06-12 18:06:01 +0000

    Update configBD.php

--- APIRest/includes/configBD.php
+++ APIRest/includes/configBD.php
@@ -1,16 +1,8 @@
 <?php
-/* 
-  //000webhost
   $servername = "localhost";
-  $username = "id5661954_agroprecios";
-  $password = "2508Agro.5";
-  $database = "id5661954_ap1";
-*/
-  //x10Hosting
-  $servername = "localhost";
-  $username = "almeriac_miguel";
-  $password = "2508Agro.5";
-  $database = "almeriac_01";
+  $username = MYSQL_USER;
+  $password = MYSQL_PASSWORD;
+  $database = MYSQL_DATABASE;
 
   try {
     $conn = new mysqli($servername, $username, $password, $database);
