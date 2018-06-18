7d962a061d7601f62583b5e8ebac96edb420ce21
m.tarifa.agro <m.tarifa.agro@gmail.com>
2018-06-18 19:20:35 +0200

    a

--- APIRest/v1/empresas.php
+++ APIRest/v1/empresas.php
@@ -9,7 +9,6 @@
 
   echo 'PHP_AUTH_USER -> ' . $_SERVER['PHP_AUTH_USER'];
   echo ' - PHP_AUTH_PW -> ' . $_SERVER['PHP_AUTH_PW'];
-  echo ' - PHP_AUTH_PW -> ' . $_SERVER['PHP_AUTH_PW'];
 
   $auth = new Authenticate();
 
