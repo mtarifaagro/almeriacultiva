e373de078dd9e84c7102a18bba170f658c488394
m.tarifa.agro <m.tarifa.agro@gmail.com>
2018-06-19 18:33:11 +0200

    sobraban cosas

--- APIRest/includes/authenticated.php
+++ APIRest/includes/authenticated.php
@@ -9,7 +9,6 @@
       if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
         $ha = base64_decode( substr($_SERVER['HTTP_AUTHORIZATION'],6) );
         list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', $ha);
-        unset $ha;
       }
 
       if (!isset($_SERVER['PHP_AUTH_USER'])) {
