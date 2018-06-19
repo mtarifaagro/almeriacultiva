3eddda89a761133df2a429fc5d0e058d9a748f25
m.tarifa.agro <m.tarifa.agro@gmail.com>
2018-06-19 18:51:14 +0200

    se quita la autenticacion

--- APIRest/includes/authenticated.php
+++ APIRest/includes/authenticated.php
@@ -6,6 +6,8 @@
     var $sql;
 
     function auth($conn){
+      return 1;
+      /*
       if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
         $ha = base64_decode( substr($_SERVER['HTTP_AUTHORIZATION'],6) );
         list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', $ha);
@@ -22,6 +24,7 @@
         
         return $res2->num_rows;
       }
+      */
     }
   }
 ?>
