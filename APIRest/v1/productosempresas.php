713a51ff15a5eeabbaa94d0ecf5ab7c7dc3a6482
m.tarifa.agro <m.tarifa.agro@gmail.com>
2018-06-19 19:15:30 +0200

    sobraban cosas

--- APIRest/v1/productosempresas.php
+++ APIRest/v1/productosempresas.php
@@ -3,8 +3,7 @@
   header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
   header("Access-Control-Allow-Headers: X-Requested-With, Origin, Content-Type, X-Auth-Token, Authorization, Accept");
   header("Content-Type: application/json");
-  header('Cache-Control: no-cache, must-revalidate, max-age=0');
-
+  
   include_once '../includes/configBD.php';
   include_once '../includes/authenticated.php';
 
