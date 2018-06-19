92369b6a5d1870a274513d3873e895f0ea946f93
m.tarifa.agro <m.tarifa.agro@gmail.com>
2018-06-19 21:56:48 +0200

    hortalizas

--- APIRest/v1/hortalizas2.php
+++ APIRest/v1/hortalizas2.php
@@ -12,15 +12,28 @@
   $numRows = $auth->auth($conn);
   if ($numRows == 1){
     $result = array();
-    $res = $conn->query("SELECT * FROM `Productos` Where `Productos`.`pro_activo` = 'Y' ");
+    $res = $conn->query("SELECT emp_id, emp_nombre, emp_imagen
+                         FROM Empresas
+                         Where emp_activo = 'Y' ");
     while($f = $res->fetch_object()){
-      $result[] = array("id" => $f->pro_id, 
-                        "nombre" => $f->pro_nombre, 
-                        "imagen" => $f->pro_imagen); 
+      $result[] = array("id" => $f->emp_id, 
+                        "nombre" => $f->emp_nombre, 
+                        "imagen" => $f->emp_imagen); 
     }
     $json = array("status" => 0, "info" => $result);
  
     echo json_encode($json);
+
+    $result2 = array();
+    $res = $conn->query("SELECT `pro_id`, `pro_nombre`, `pro_imagen` FROM `Productos2` Where `pro_activo` = 'Y' ");
+    while($f = $res->fetch_object()){
+      $resul2t[] = array("id" => $f->pro_id, 
+                        "nombre" => $f->pro_nombre, 
+                        "imagen" => $f->pro_imagen); 
+    }
+    $json2 = array("status" => 0, "info" => $result2);
+ 
+    echo json_encode($json2);
   } else {
     header('WWW-Authenticate: Basic realm="LOGIN REQUIRED"');
     header('HTTP/1.0 401 Unauthorized');
