a93dee4d4e807f5255c1b086a8ce78126f83f63b
m.tarifa.agro <m.tarifa.agro@gmail.com>
2018-06-19 18:03:08 +0200

    se añade cabeceras http_authorization

--- APIRest/includes/authenticated.php
+++ APIRest/includes/authenticated.php
@@ -6,6 +6,12 @@
     var $sql;
 
     function auth($conn){
+      if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+        $ha = base64_decode( substr($_SERVER['HTTP_AUTHORIZATION'],6) );
+        list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', $ha);
+        unset $ha;
+      }
+
       if (!isset($_SERVER['PHP_AUTH_USER'])) {
         return -1;
       } else{
