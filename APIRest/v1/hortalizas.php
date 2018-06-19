4780c73e47feb097f79fbde684a1a2ff38f62b03
m.tarifa.agro <m.tarifa.agro@gmail.com>
2018-06-19 19:09:21 +0200

    sobraban cosas

--- APIRest/v1/hortalizas.php
+++ APIRest/v1/hortalizas.php
@@ -1,8 +1,9 @@
-<?php
+<?php 
   header("Access-Control-Allow-Origin: *");
   header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
   header("Access-Control-Allow-Headers: X-Requested-With, Origin, Content-Type, X-Auth-Token, Authorization, Accept");
   header("Content-Type: application/json");
+  header('Cache-Control: no-cache, must-revalidate, max-age=0');
 
   include_once '../includes/configBD.php';
   include_once '../includes/authenticated.php';
