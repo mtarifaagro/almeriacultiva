c354457e5f0b9e5cc930ce85e8a7411e49106cde
mtarifaagro <40060725+mtarifaagro@users.noreply.github.com>
2018-07-02 14:12:38 +0000

    Update hortalizas.php

--- APIRest/v1/hortalizas.php
+++ APIRest/v1/hortalizas.php
@@ -2,7 +2,7 @@
   header("Access-Control-Allow-Origin: *");
   header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
   header("Access-Control-Allow-Headers: X-Requested-With, Origin, Content-Type, X-Auth-Token, Authorization, Accept");
-  header("Content-Type: application/json");
+  header("Content-Type: application/json;charset=utf-8");
 
   include_once '../includes/configBD.php';
   include_once '../includes/authenticated.php';
