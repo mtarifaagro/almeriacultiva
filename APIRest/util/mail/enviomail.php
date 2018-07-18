6d0389ad461f0837270c36d5dd192b95bc29b5c8
mtarifaagro <miguel.tarifa@aireuropa.com>
2018-07-18 14:06:44 +0200

    nuevo mail

---
+++ APIRest/util/mail/enviomail.php
@@ -0,0 +1,22 @@
+<?php 
+require_once 'lib/swift_required.php';
+
+$myEmail = "m.tarifa.agro@gmail.com"; 
+$asunto = "Este mensaje es de prueba"; 
+
+// make sure you get these SMTP settings right
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl") 
+    ->setUsername($myEmail)
+    ->setPassword('2508Agro.5');
+
+$mailer = Swift_Mailer::newInstance($transport);
+
+// the message itself
+$message = Swift_Message::newInstance('email subject')
+    ->setFrom(array('noreply@AlmeriaCultiva.com' => 'no reply'))
+    ->setTo(array($myEmail))
+    ->setBody("email body");
+
+$result = $mailer->send($message);
+
+?>
