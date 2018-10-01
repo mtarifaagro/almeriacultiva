c702c73cb0fe831d01c5149c1d0a062a9f1d9cea
Miguel Tarifa Fernández <migueltarifafernandez@iMac-de-Miguel.local>
2018-10-01 19:20:36 +0200

    Cabeceras de prueba

--- APIRest/includes/prueba.php
+++ APIRest/includes/prueba.php
@@ -1,4 +1,9 @@
 <?php 
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  header("Access-Control-Allow-Headers: X-Requested-With, Origin, Content-Type, X-Auth-Token, Authorization, Accept");
+  header("Content-Type: application/json");
+  
   $servername = getenv('MYSQL_SERVICE_HOST'); 
   $database = getenv('MI_DATABASE_NAME');
   $username = getenv('MI_DATABASE_USER');
