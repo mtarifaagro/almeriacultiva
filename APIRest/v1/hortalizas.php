5f0c062c30d06a83ef9db3d196ccd5cc32daea15
Miguel Tarifa Fernández <migueltarifafernandez@iMac-de-Miguel.local>
2019-02-13 19:20:11 +0100

    a

--- APIRest/v1/hortalizas.php
+++ APIRest/v1/hortalizas.php
@@ -6,24 +6,24 @@
 
   include_once '../includes/configBD.php';
   include_once '../includes/authenticated.php';
-  
+  echo 'prueba';
   $auth = new Authenticate();
   $numRows = $auth->auth($conn);
   if ($numRows == 1){
     $result = array();
     try {
-    $res = $conn->query("SELECT pro_id, pro_nombre, pro_imagen 
-                         FROM Productos 
-                         WHERE pro_activo = 'Y' ");
-    while($f = $res->fetch_object()){
-      $result[] = array("id" => $f->pro_id, 
-                        "nombre" => $f->pro_nombre, 
-                        "imagen" => $f->pro_imagen); 
-    }
-    $json = array("status" => 0, "info" => $result);
-    echo json_encode($json);
+      $res = $conn->query("SELECT pro_id, pro_nombre, pro_imagen 
+                           FROM Productos 
+                           WHERE pro_activo = 'Y' ");
+      while($f = $res->fetch_object()){
+        $result[] = array("id" => $f->pro_id, 
+                          "nombre" => $f->pro_nombre, 
+                          "imagen" => $f->pro_imagen); 
+      }
+      $json = array("status" => 0, "info" => $result);
+      echo json_encode($json);
     } catch (Exception $e) {
-      echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+      echo 'Excepción capturada: ',  $e->getMessage(), '\n';
     }
   } else {
     header('WWW-Authenticate: Basic realm="LOGIN REQUIRED"');
